ecdf7f15d86623600b462bd2c9fffe6af6876108
MattPeete1 <mattpeete@gmail.com>
2023-08-22 08:27:16 -0400

    Added a little functionality with logic

--- js/main.js
+++ js/main.js
@@ -8,30 +8,35 @@ const slotOptions = [
     '💸'
 ];
 
-const walletEl = document.getElementById('.wltMoney');
-
-let wheelEls = document.querySelectorAll('.wheel');
-
-wheelEls.forEach(function(el, idx) {
-    el.innerText = slotOptions[idx % slotOptions.length];
-});
-
-
-
-
-
-
-
 /*----- state variables -----*/
 
 
 /*----- cached elements  -----*/
-
+let wheelEls = document.querySelectorAll('.wheel');
+let winMsgEl = document.getElementById('winMsg');
+let walletEl = document.getElementById('wltMoney');
+let betEl = document .getElementById('betMoney');
 
 /*----- event listeners -----*/
-
+document.getElementById('spin').addEventListener('click', handleLeverPull);
 
 /*----- functions -----*/
 function init() {
+    render();
+};
 
+function render() {
+
+};
+
+function handleLeverPull() {
+    wheelEls.forEach(function(el) {
+        let choice = Math.floor(Math.random() * slotOptions.length);
+        el.innerText = slotOptions[choice];
+    });
+    if (w1.innerText === w2.innerText && w2.innerText === w3.innerText && w3.innerText === w1.innerText) {
+        return winMsgEl.innerText = 'You Won Money'
+    } else {
+        return winMsgEl.innerText = 'Try Again!'
+    }
 };
