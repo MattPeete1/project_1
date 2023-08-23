645e2e4f2dbc324be6a20874f1f3d216745a8144
MattPeete1 <mattpeete@gmail.com>
2023-08-23 02:26:13 -0400

    Made the betting buttons along with the wallet work together

--- js/main.js
+++ js/main.js
@@ -8,25 +8,35 @@ const slotOptions = [
     '💸'
 ];
 
+const strawBry = 1.50;
+const bannana = 1.60;
+const grapes = 1.70;
+const cherry = 1.80;
+const money = 2.50;
 /*----- state variables -----*/
 
 
 /*----- cached elements  -----*/
 let wheelEls = document.querySelectorAll('.wheel');
 let winMsgEl = document.getElementById('winMsg');
-let walletEl = document.getElementById('wltMoney');
-let betEl = document .getElementById('betMoney');
-
+let wallet = document.getElementById('wltMoney');
+let bet = document.getElementById('betMoney');
 /*----- event listeners -----*/
 document.getElementById('spin').addEventListener('click', handleLeverPull);
-
+document.getElementById('betUp').addEventListener('click', handleBettingUp);
+document.getElementById('betDown').addEventListener('click', handleBettingDown);
 /*----- functions -----*/
+init();
+
 function init() {
-    render();
+    wallet.innerHTML = Number(500);
+    bet.innerHTML = Number(0);
+    render()
 };
 
 function render() {
-
+    handleBettingUp();
+    handleBettingDown();
 };
 
 function handleLeverPull() {
@@ -35,8 +45,27 @@ function handleLeverPull() {
         el.innerText = slotOptions[choice];
     });
     if (w1.innerText === w2.innerText && w2.innerText === w3.innerText && w3.innerText === w1.innerText) {
-        return winMsgEl.innerText = 'You Won Money'
+        return winMsgEl.innerText = 'You Won Money';
     } else {
-        return winMsgEl.innerText = 'Try Again!'
-    }
+        return winMsgEl.innerText = 'Try Again!';
+    };
 };
+
+function handleBettingUp() {
+    bet.innerHTML = Number(bet.innerHTML) + 10;
+    if (bet.innerHTML > 100) {
+        bet.innerHTML = 100;
+    } if (Number(bet.innerHTML) + 10) {
+        wallet.innerHTML = Number(wallet.innerHTML) + Number(-10);
+    };
+};
+
+function handleBettingDown() {
+    bet.innerHTML = Number(bet.innerHTML) - 10;
+    if (bet.innerHTML < 10) {
+        bet.innerHTML = 10;
+    } if (Number(bet.innerHTML) - 10) {
+        wallet.innerHTML = Number(wallet.innerHTML) + Number(10);
+    };
+};
+
